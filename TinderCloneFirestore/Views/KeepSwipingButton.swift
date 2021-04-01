2f79c42337992e5f45549ef6c49943dccb8aadf7
Pradeep Gianchandani <pgianchandani@zipcar.com>
2021-04-01 13:43:51 +0530

    - Add match view components - Create gradient layer buttons

---
+++ TinderCloneFirestore/Views/KeepSwipingButton.swift
@@ -0,0 +1,46 @@
+//
+//  KeepSwipingButton.swift
+//  TinderCloneFirestore
+//
+//  Created by Pradeep Gianchandani on 01/04/21.
+//
+
+import UIKit
+
+// Create button with gradient color border
+class KeepSwipingButton: UIButton {
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        let gradientLayer = CAGradientLayer()
+        let leftColor = #colorLiteral(red: 1, green: 0.01176470588, blue: 0.4470588235, alpha: 1)
+        let rightColor = #colorLiteral(red: 1, green: 0.3294117647, blue: 0.3176470588, alpha: 1)
+        gradientLayer.colors = [leftColor.cgColor, rightColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
+        
+        let cornerRadius = rect.height / 2
+        let maskLayer = CAShapeLayer()
+        
+        let maskPath = CGMutablePath()
+        maskPath.addPath(UIBezierPath(roundedRect: rect, cornerRadius: cornerRadius).cgPath)
+        
+        // Punch out the middle
+        // Here we specify the line width of 2 points by setting the inset properties
+        maskPath.addPath(UIBezierPath(roundedRect: rect.insetBy(dx: 2, dy: 2), cornerRadius: cornerRadius).cgPath)
+        
+        maskLayer.path = maskPath
+        maskLayer.fillRule = .evenOdd
+        
+        gradientLayer.mask = maskLayer
+        
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        
+        gradientLayer.frame = rect
+        
+        layer.cornerRadius = cornerRadius
+        clipsToBounds = true
+    }
+
+}
